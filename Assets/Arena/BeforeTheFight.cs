f14a6b119f8c93c323ea34aa1b78f2b6fa7f6639
rycon2424 <rycon2424@users.noreply.github.com>
2018-10-04 13:51:36 +0200

    Whole NameSystem Working

--- Assets/Arena/BeforeTheFight.cs
+++ Assets/Arena/BeforeTheFight.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class BeforeTheFight : MonoBehaviour
@@ -8,8 +9,14 @@ public class BeforeTheFight : MonoBehaviour
     [Header("Disabled on duels")]
     public GameObject mainCamera;
     public GameObject uiCanvas;
+    public Text playerName, playerNameC;
+    public Text enemyName, enemyNameC, bossNameC;
+    public string enemysFullname;
+    public string[] firstNames = new string[21];
+    public string[] surNames = new string[21];
 
     [Header("On Tournaments")]
+    public string BossName;
     public GameObject duelCanvas;
     public GameObject tournamentCanvas;
     public GameObject bossFightOst;
@@ -20,6 +27,7 @@ public class BeforeTheFight : MonoBehaviour
     {
         if (!isTournament)
         {
+
             bossFightOst.SetActive(false);
             duelCanvas.SetActive(true);
             tournamentCanvas.SetActive(false);
@@ -29,6 +37,7 @@ public class BeforeTheFight : MonoBehaviour
             tournamentCanvas.SetActive(true);
             duelCanvas.SetActive(false);
         }
+        NameGen();
         mainCamera.SetActive(false);
         uiCanvas.SetActive(false);
     }
@@ -38,8 +47,23 @@ public class BeforeTheFight : MonoBehaviour
         bossFightOst.SetActive(true);
     }
 
+    public void NameGen()
+    {
+        if (isTournament)
+        {
+            bossNameC.text = BossName;
+            enemyName.text = BossName;
+        }
+        if (!isTournament)
+        {
+            enemysFullname = firstNames[Random.Range(1, firstNames.Length)] + " " + surNames[Random.Range(1, surNames.Length)];
+            enemyName.text = enemysFullname;
+        }
+        playerName.text = CharacterCreation.username;
+    }
+
     void Update()
-    { 
+    {
 
     }
 
@@ -55,5 +79,14 @@ public class BeforeTheFight : MonoBehaviour
         }
         mainCamera.SetActive(true);
         uiCanvas.SetActive(true);
+        if (!isTournament)
+        {
+            enemyNameC.text = enemysFullname;
+        }
+        if (isTournament)
+        {
+            enemyNameC.text = BossName;
+        }
+        playerNameC.text = CharacterCreation.username;
     }
 }
