bf1142165f2daab329dd227fbd11d67f6243afaf
Bosko Ivkovic <bosko2003414@gmail.com>
2018-10-06 22:32:32 +0200

    Allay/NightDayCycle/ItemListScroll

---
+++ Assets/Scripts/Alleyway.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alleyway : MonoBehaviour {
+
+    public Text info;
+    public GameObject Canvas;
+    public GameObject villageCamera;
+    public GameObject glow;
+    
+	void Start ()
+    {
+        Canvas.SetActive(false);
+        glow.SetActive(false);
+    }
+    
+    void OnMouseOver()
+    {
+        glow.SetActive(true);
+        if (!ShopOpener.inShop)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                villageCamera.SetActive(false);
+                Canvas.SetActive(true);
+                info.text = "Nothing here maybe I should return later.";
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        glow.SetActive(false);
+    }
+
+    public void ReturntoVillage()
+    {
+        Canvas.SetActive(false);
+        villageCamera.SetActive(true);
+    }
+
+}
---
+++ Assets/Village/NextList.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextList : MonoBehaviour {
+
+    public GameObject[] itemLists = new GameObject[3];
+    public int currentList = 0;
+
+    public GameObject forward, back;
+
+    private void Start()
+    {
+        itemLists[currentList].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (currentList == itemLists.Length - 1)
+        {
+            forward.SetActive(false);
+        }
+        else
+        {
+            forward.SetActive(true);
+        }
+
+        if (currentList == 0)
+        {
+            back.SetActive(false);
+        }
+        else
+        {
+            back.SetActive(true);
+        }
+    }
+
+    public void BackToListOne()
+    {
+        itemLists[currentList].SetActive(false);
+        currentList = 0;
+        itemLists[currentList].SetActive(true);
+    }
+
+    public void Next()
+    {
+        itemLists[currentList].SetActive(false);
+        currentList++;
+        itemLists[currentList].SetActive(true);
+    }
+
+    public void Back()
+    {
+        currentList--;
+        itemLists[currentList + 1].SetActive(false);
+        itemLists[currentList].SetActive(true);
+    }
+
+}
