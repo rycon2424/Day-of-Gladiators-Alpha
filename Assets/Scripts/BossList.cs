157f8f9cdf6bfb14b26e26a59b0d6c4537637cd8
Bosko Ivkovic <bosko2003414@gmail.com>
2018-10-14 18:32:09 +0200

    Bosslist

---
+++ Assets/Scripts/BossList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossList : MonoBehaviour {
+
+    public GameObject ColUI;
+    public GameObject mainCamera;
+    public GameObject list;
+
+	
+    public void ShowBosses()
+    {
+        ColUI.SetActive(false);
+        mainCamera.SetActive(false);
+        list.SetActive(true);
+    }
+
+    public void GoBack()
+    {
+        ColUI.SetActive(true);
+        mainCamera.SetActive(true);
+        list.SetActive(false);
+    }
+
+}
